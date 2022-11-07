95a5f10290d35685ed6cbdccc40f40fb055c164c
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-11-07 18:10:47 -0500

    post tutoring session

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,72 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins* for a service worker* and manifest file*.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new WebpackPwaManifest({
+          name: 'Text-Editor Application',
+          short_name: 'Text boxes',
+          description: 'Keep track of important texts!',
+          background_color: '#7eb4e2',
+          theme_color: '#7eb4e2',
+          start_url: './',
+          publicPath: './',
+          fingerprints: false,
+          inject: true,
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            }
+          ],
+        }), 
+        new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'src-sw.js',
+        }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
